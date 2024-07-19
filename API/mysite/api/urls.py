6ecbad4dd0400e1e2911204882289345a7e190db
Mauricio Fuentes Gomez <54040778+MFG-art@users.noreply.github.com>
2024-07-19 13:55:30 -0500

    Updating urls

--- API/mysite/api/urls.py
+++ API/mysite/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
-    path("blogpost/", views.BlogPostViewSet.as_view({'get': 'list'}), name="blogpost-view-create"),
+    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
     path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update" )
 ]
